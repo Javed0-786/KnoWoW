e84b126281cb1f95996a0813b28844e122d5880c
Javed Alam <alam9120573582@gmail.com>
2024-04-28 02:10:00 +0530

    Home Screen Layout Done

--- app/src/main/java/com/example/knowow/MainActivity.kt
+++ app/src/main/java/com/example/knowow/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.knowow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
