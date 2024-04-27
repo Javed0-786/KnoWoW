a0f7c5638209b93acac5fe2ea7f9dd10939948db
Javed Alam <alam9120573582@gmail.com>
2024-04-28 02:44:36 +0530

    Splash Screen added and The app icon also added

--- app/src/main/java/com/example/knowow/MainActivity.kt
+++ app/src/main/java/com/example/knowow/MainActivity.kt
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
+
+    fun scienceCard(view: View) {
+
+    }
+    fun historicalCard(view: View) {}
+    fun technologyCard(view: View) {}
+    fun natureCard(view: View) {}
+    fun sportsCard(view: View) {}
+    fun generalCard(view: View) {}
+    fun codingCard(view: View) {}
+    fun geographyCard(view: View) {}
 
 }
